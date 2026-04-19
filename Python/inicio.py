a6b8150bffde9a0d1b121779c774c96b7a704812
AndresAlvarado <aaap2005@gmail.com>
2026-04-19 16:39:44 -0500

    cambios en rama python

---
+++ Python/inicio.py
@@ -0,0 +1,30 @@
+class clases:
+    def __init__(self, curso, profe, dia1, dia2):
+        self.curso = curso
+        self.profe = profe
+        self.dia1 = dia1
+        self.dia2 = dia2
+        
+lista_clases = []
+lista_horario = []
+
+clase1 = clases("funda elec", "huaman negron", ["martes", 14, 16], ["jueves", 14, 16])
+clase2 = clases("SO", "heiner", ["martes", 7, 10], ["jueves", 7, 10])
+clase3 = clases("poo", "nina hanco", ["lunes", 7, 9], ["miercoles", 7, 10])
+clase4 = clases("estadistica", "miguel", ["martes", 11, 13], ["jueves", 10, 13])
+
+
+lista_clases.append(clase1)
+lista_clases.append(clase2)
+lista_clases.append(clase3)
+lista_clases.append(clase4)
+
+
+def dia():
+    for x in lista_clases: 
+         
+        
+        
+        
+
+
